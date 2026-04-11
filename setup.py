6faf9c37cbdb03cf7eb13c9c70a698f8aa18e1e2
Milan Hauth <milahu@milahu.duckdns.org>
2026-04-11 21:44:09 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="binarize-pdf",
+    version="0.1.0",
+    description="Convert PDF to black and white using adaptive thresholding",
+    py_modules=[],
+    scripts=[
+        "binarize-pdf.py",
+    ],
+    install_requires=[
+        "pdf2image",
+        "opencv-python",
+        "numpy",
+        "Pillow",
+        "tqdm",
+    ],
+    python_requires=">=3.8",
+)
